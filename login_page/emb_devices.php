022b66434b9c262b5bb701e4f236052ed24e7b25
Yohan <yohanjasanka0044@gmail.com>
2015-12-17 01:36:48 +0530

    Update driver page

--- login_page/emb_devices.php
+++ login_page/emb_devices.php
@@ -114,7 +114,7 @@
                                         </form>
                                     </div>
                                     <div id="update" class="tab-pane fade">
-                                        <h5>Update existind device details</h5>
+                                        <h5>Update existing device details</h5>
 
                                         <form class="form-horizontal" role="form" method="post" action="update_emb_device.php">
                                             <div class="form-group">
