03ad85ad22003c1cc19572ac3c09859a96c3c90b
Yohan <yohanjasanka0044@gmail.com>
2016-01-09 18:41:29 +0530

    Added submit button

--- login_page/packages.php
+++ login_page/packages.php
@@ -138,6 +138,11 @@
                                                     <input type="text" class="form-control" name="price-per-day" placeholder="">
                                                 </div>
                                             </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-2 col-sm-10">
+                                                    <button type="submit" class="btn btn-default">Submit</button>
+                                                </div>
+                                            </div>
                                         </form>
 
                                     </div>
@@ -166,6 +171,11 @@
                                                 <label class="control-label col-sm-2" for="price-per-day">Price per day:</label>
                                                 <div class="col-sm-1">
                                                     <input type="text" class="form-control" name="price-per-day" placeholder="">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-2 col-sm-10">
+                                                    <button type="submit" class="btn btn-default">Submit</button>
                                                 </div>
                                             </div>
                                         </form>
